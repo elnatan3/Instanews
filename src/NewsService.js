37516e07d8b17a752f9e0b5930560fd571f0462e
Elnatan <elnatanmesfintesfa20@augustana.edu>
2023-11-13 09:42:00 -0600

    created environmental variable

--- src/NewsService.js
+++ src/NewsService.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const API_KEY = 'a422825d9b7644fd91f1b20cc4133d13'; // Replace with your API key
+// Access the API key from environment variables
+const API_KEY = process.env.NEWS_API_KEY;
 
 export const fetchNews = async (category) => {
     try {
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?category=${category}&language=en&apiKey=${API_KEY}`);
-      return response.data.articles;
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?category=${category}&language=en&apiKey=${API_KEY}`);
+        return response.data.articles;
     } catch (error) {
-      console.error('Error fetching news:', error);
+        console.error('Error fetching news:', error);
     }
-  };
+};
