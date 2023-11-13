4411e3a36ba7792169df545086b6d5e9ac3b15ee
Elnatan <elnatanmesfintesfa20@augustana.edu>
2023-11-13 09:47:21 -0600

    made a small change

--- src/NewsService.js
+++ src/NewsService.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
-// Access the API key from environment variables
-const API_KEY = process.env.NEWS_API_KEY;
+const API_KEY = 'a422825d9b7644fd91f1b20cc4133d13'; // Replace with your API key
 
 export const fetchNews = async (category) => {
     try {
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?category=${category}&language=en&apiKey=${API_KEY}`);
-        return response.data.articles;
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?category=${category}&language=en&apiKey=${API_KEY}`);
+      return response.data.articles;
     } catch (error) {
-        console.error('Error fetching news:', error);
+      console.error('Error fetching news:', error);
     }
-};
+  };
